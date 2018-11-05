ea6ef41625961ee830234f019afb27b6e233baae
Kautzar Alibani <kautzar.alibani@modalrakyat.id>
2018-11-05 23:37:29 +0700

    Create page login

---
+++ src/pages/Login.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<b-container class="bv-example-row ">
+			<b-row class="justify-content-md-center">
+				<b-form @submit="onSubmit" class="text-left custom-form">
+      		<b-form-group label="Username">
+						<b-form-input
+							type="text"
+							v-model="payload.username"
+							required
+							placeholder="Enter Username">
+						</b-form-input>
+      		</b-form-group>
+					<b-form-group label="Password">
+						<b-form-input
+							type="password"
+							v-model="payload.password"
+							required
+							placeholder="Enter password">
+						</b-form-input>
+      		</b-form-group>
+      		<b-button type="submit" variant="primary">Submit</b-button>
+    		</b-form>
+			</b-row>
+	</b-container>	
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			payload : {
+				username: '',
+				password: ''
+			}
+		}
+	}
+
+}
+</script>
+
+<style lang="scss" scoped>
+	.custom-form {
+		border: 1px solid gainsboro;
+		border-radius: 4px;
+		min-width: 300px;
+		padding: 20px; 
+	}
+</style>
